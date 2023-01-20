c5c31b0e9b801bf9707ade916fdf7e4b4d763b0c
Ching Cheng <ching@chings-air.lan>
2023-01-19 18:26:26 -0800

    get and post done

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -12,16 +12,18 @@ class LocationVOEncoder(ModelEncoder):
         "closet_name",
         "section_number",
         "shelf_number",
-
+        "import_href",
     ]
 
 class HatListEncoder(ModelEncoder):
     model = Hat
     properties = [
-        "id",
         "style_name",
+        "id",
     ]
 
+    def get_extra_data(self, o):
+        return {"location": o.location.closet_name}
 
 class HatDetailEncoder(ModelEncoder):
     model = Hat
@@ -33,24 +35,50 @@ class HatDetailEncoder(ModelEncoder):
         "location",
     ]
     encoders = {
-        "location": LocationVOEncoder
+        "location": LocationVOEncoder()
     }
 
 @require_http_methods(["GET", "POST"])
-def list_hats(request):
+def list_hats(request, location_vo_id=None):
     if request.method == "GET":
-        hats = Hat.objects.all()
-
+        if location_vo_id is not None:
+            hats = Hat.object.filter(location=location_vo_id)
+        else:
+            hats = Hat.objects.all()
         return JsonResponse (
             {"hats": hats},
             encoder=HatListEncoder,
         )
 
     else:
-        content = json.load(request.body)
+        content = json.loads(request.body)
+
+        try:
+            location_href = content["location"]
+            location = LocationVO.objects.get(import_href=location_href)
+            content["location"] = location
+        except LocationVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+
         hat = Hat.objects.create(**content)
         return JsonResponse(
             hat,
             encoder=HatDetailEncoder,
             safe=False,
         )
+
+
+# @require_http_methods(["DELETE", "GET", "PUT"])
+# def show_hat(request, id):
+#     if request.method == "GET":
+#         hat = Hat.objects.get(id=id)
+#         return JsonResponse(
+#             hat,
+#             encoder=HatDetailEncoder,
+#             safe=False,
+#         )
+#     elif request.method == "DELETE":
+#         count, _ = Hat.objects.filter
